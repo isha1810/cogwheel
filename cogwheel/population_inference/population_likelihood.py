472885048fa630dd7025dcfe8592a813c075b993
Isha Anantpurkar <isha@braid2.cnsi.ucsb.edu>
2024-04-12 10:04:16 -0700

    changes to make sampling work

--- cogwheel/population_inference/population_likelihood.py
+++ cogwheel/population_inference/population_likelihood.py
@@ -16,17 +16,17 @@ class PopulationLikelihood(utils.JSONMixin):
     population model to compute the likelihood of GW events coming
     from a particular model.
     """
-    def __init__(self, parameterized_population, injections_summary, all_pe_samples, R0, injection_population_model):
+    def __init__(self, parametrized_population, injections_summary, all_pe_samples, R0, injection_population_model):
         """
         Parameters
         ----------
     
         """
-        self.parameterized_population = parameterized_population
-        self.injection_population_model = injection_population_model
+        self.parametrized_population = parametrized_population
         self.pastro_func = injections_summary['pastro_func']
         self.recovered_injections = injections_summary['recovered_injections']
         self.Ninj = injections_summary['Ninj']
+        self.injection_population_model = injection_population_model
         self.all_pe_samples = all_pe_samples
         self.R0 = R0
     
@@ -45,15 +45,18 @@ class PopulationLikelihood(utils.JSONMixin):
         """
         Returns Eq. 17
         """
-        pe_param_keys = np.array(self.parameterized_population.sampled_params) # lnprior function takes sampled params as input
+        pe_param_keys = np.array(self.parametrized_population.sampled_params) # lnprior function takes sampled params as input
         inj_param_keys = np.array(self.injection_population_model.sampled_params)
         
         #params_df = self.recovered_injections[param_keys]
         #all_params_dict = {col: params_df[col].to_numpy() for col in params_df.columns}
         #all_params_dict.update(hyperparams_dic)
         
-        vectorized_population_lnprior = np.vectorize(self.parameterized_population.lnprior)
-        vectorized_injection_population_lnprior = np.vectorize(self.injection_population_model.lnprior)
+        #vectorized_population_lnprior = np.vectorize(self.parametrized_population.lnprior)
+        #vectorized_injection_population_lnprior = np.vectorize(self.injection_population_model.lnprior)
+
+        vectorized_population_lnprior = self.parametrized_population.lnprior
+        vectorized_injection_population_lnprior = self.injection_population_model.lnprior
         
         w_arr = np.zeros(len(self.all_pe_samples))
         for i, pe_samples in enumerate(self.all_pe_samples):
@@ -76,14 +79,26 @@ class PopulationLikelihood(utils.JSONMixin):
         Returns population averaged sensitive volume time (Eq. 18)
         """
         norm_fac = 1 #CHANGE THIS TO CORRECT VALUE
-        param_keys = np.array(self.parameterized_population.sampled_params)
+        param_keys = np.array(self.parametrized_population.sampled_params)
         params_df = self.recovered_injections[param_keys]
         all_params_dict = {col: params_df[col].to_numpy() for col in params_df.columns}
         all_params_dict.update(hyperparams_dic)
 
-        vectorized_population_lnprior = np.vectorize(self.parameterized_population.lnprior)
+        #vectorized_population_lnprior = np.vectorize(self.parametrized_population.lnprior)
         #vectorized_injection_population_lnprior = np.vectorize(self.injection_population_model.lnprior)
+
+        vectorized_population_lnprior = self.parametrized_population.lnprior
         
         VT = (1/self.Ninj) * np.sum(np.exp(vectorized_population_lnprior(**all_params_dict) - 
-                                    self.recovered_injections['lnprior']*norm_fac))
+                                    self.recovered_injections['ln_pdet_fid']*norm_fac))
         return VT
+
+    def get_init_dict(self):
+        init_dict = {'parametrized_population': self.parametrized_population,
+                     'injections_summary_fname': 'placeholder_name',
+                     'all_pe_samples_path':'placeholder_name',
+                     'R0':self.R0,
+                     'injection_population_model':self.injection_population_model}
+        return init_dict
+                     
+        

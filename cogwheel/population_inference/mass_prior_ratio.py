42a2e6ced9684962ebfba224c8df401ec71c4dc3
Isha Anantpurkar <isha@braid2.cnsi.ucsb.edu>
2024-08-29 13:07:21 -0700

    edited function in pop_utils and fixed bug in mass_prior_ratio

--- cogwheel/population_inference/mass_prior_ratio.py
+++ cogwheel/population_inference/mass_prior_ratio.py
@@ -6,7 +6,7 @@ from .base_prior_ratio import PriorRatio
 from cogwheel.cosmology import z_of_d_luminosity, comoving_to_luminosity_diff_vt_ratio
 from cogwheel.prior import IdentityTransformMixin, Prior
 
-from .pop_utils import normalized_powerlaw_distribution #,normalized_truncated_gaussian_distribution
+from .pop_utils import normalized_powerlaw_distribution, smoothing_function #,normalized_truncated_gaussian_distribution
 
 
 class TruncatedMassModelToVolumetricPrior(PriorRatio):
@@ -29,7 +29,7 @@ class TruncatedMassModelToVolumetricPrior(PriorRatio):
         q_min = m_min/m1_source
         q_max = 1.
         q_lnp = np.log(normalized_powerlaw_distribution(q.values, beta_q, q_min.values, q_max))
-        pop_lnp = mass_lnp + q_lnp
+        pop_lnp = mass_lnp + q_lnp + np.log(comoving_to_luminosity)
         
         ivs_mass_jacobian = 2*np.log(1+z) + np.log(m1_source)
         ivs_mass_lnp = ivs_mass_jacobian
@@ -39,30 +39,62 @@ class TruncatedMassModelToVolumetricPrior(PriorRatio):
 
 class TruncatedMassModelHyperPrior(IdentityTransformMixin, Prior):
     standard_params = ['rate', 'alpha', 'm_min', 'm_max', 'beta_q']
-    range_dic={'rate':(60, 200),'alpha':(-4, 6), 'm_min':(2,10), 'm_max':(50, 100), 'beta_q':(-4,6)}
+    range_dic={'rate':(5, 200),'alpha':(-4, 12), 'm_min':(2,10), 'm_max':(30, 100), 'beta_q':(-4,12)}
     def lnprior(self, rate, alpha, m_min, m_max, beta_q):
         log_uniform_prior = - np.log(np.prod(self.cubesize))
         log_jeffreys_prior = - 0.5*np.log(rate)
         return log_uniform_prior + log_jeffreys_prior
 
-# class MassPowerLawPeakToIntrinsicVolumetricSpinPrior(PriorRatio):
-#     numerator = 'MassPowerLawPeak'
-#     denominator = 'IntrinsicVolumetricSpinPrior'
-#     params = ['m1_source','q']
-#     base_quantities = ['d_luminosity']
-#     derived_quantities = ['z', 'comoving_to_luminosity']
-#     hyperparams = ['lambda_peak', 'alpha', 'm_max', 'm_mean', 'm_std', 'beta_q']
 
-#     def compute_auxiliary_quantities(self, d_luminosity):
-#         aux_quantities_dataframe = pd.DataFrame()
-#         aux_quantities_dataframe['z'] = z_of_d_luminosity(d_luminosity)
-#         aux_quantities_dataframe['comoving_to_luminosity'] \
-#             = comoving_to_luminosity_diff_vt_ratio(d_luminosity)
+class MassPowerLawPeakToIntrinsicVolumetricSpinPrior(PriorRatio):
+    numerator = 'MassPowerLawPeak'
+    denominator = 'IntrinsicVolumetricSpinPrior'
+    params = ['m1_source','q']
+    base_quantities = ['d_luminosity']
+    derived_quantities = ['z', 'comoving_to_luminosity']
+    hyperparams = ['lambda_peak', 'alpha', 'm_max', 'm_mean', 'm_std', 'beta_q', 'm_min', 'delta_m']
+
+    def compute_auxiliary_quantities(self, d_luminosity):
+        aux_quantities_dataframe = pd.DataFrame()
+        aux_quantities_dataframe['z'] = z_of_d_luminosity(d_luminosity)
+        aux_quantities_dataframe['comoving_to_luminosity'] \
+            = comoving_to_luminosity_diff_vt_ratio(d_luminosity)
         
-#         return aux_quantities_dataframe
+        return aux_quantities_dataframe
 
-#     def lnprior_ratio(self, m1_source, q, z, d_luminosity, 
-#                       lambda_peak, alpha, m_max, m_mean, m_std, beta_q):
-#         mass_lnp = np.log((1-lambda_peak)*normalized_powerlaw_distribution(m1_source, alpha, 3, m_max) +
-#                          lambda_peak*normalized_truncated_gaussian_distribution(m1_source, m_mean, m_std, 3, m_max))
-#         q_lnp = 
+    def lnprior_ratio(self, m1_source, q, z, comoving_to_luminosity, 
+                      lambda_peak, alpha, m_max, m_mean, m_std, beta_q,
+                      m_min, delta_m):
+        m1_source_bounds = np.array([m_min, m_max])
+        a_transformed, b_transformed = (m1_source_bounds -
+                                        m_mean) / m_std
+        gaussian_mass_peak = np.exp(stats.truncnorm.logpdf(x=m1_source,
+                                                     a=a_transformed,
+                                                     b=b_transformed,
+                                                     loc=m_mean,
+                                                     scale=m_std))
+        mass_lnp = (np.log((1-lambda_peak)*normalized_powerlaw_distribution(m1_source.values, -alpha, m_min, m_max) +
+                         lambda_peak*gaussian_mass_peak) + np.log(smoothing_function(m1_source.values, m_min, delta_m)))
+        q_min = m_min/m1_source
+        q_max = 1.
+        m2_source = q*m1_source
+        q_lnp = (np.log(normalized_powerlaw_distribution(q.values, beta_q, q_min.values, q_max)) +
+                 np.log(smoothing_function(m2_source.values, m_min, delta_m)))
+        pop_lnp = mass_lnp + q_lnp + np.log(comoving_to_luminosity)
+
+        ivs_mass_jacobian = 2*np.log(1+z) + np.log(m1_source)
+        ivs_mass_lnp = ivs_mass_jacobian
+        ivs_lnp = ivs_mass_lnp 
+
+        return pop_lnp - ivs_lnp
+
+class PowerLawPeakMassModelHyperPrior(IdentityTransformMixin, Prior):
+    standard_params = ['rate', 'lambda_peak', 'alpha', 'm_max', 'm_mean', 
+                       'm_std', 'beta_q', 'm_min', 'delta_m']
+    range_dic={'rate':(5, 200),'lambda_peak':(0, 1), 'alpha':(-4, 12), 
+               'm_max':(30, 100), 'm_mean':(20, 50), 'm_std':(1, 10),'beta_q':(-4, 12),
+               'm_min':(2, 10), 'delta_m':(0, 10)}
+    def lnprior(self, rate, lambda_peak, alpha, m_max, m_mean, m_std, beta_q, m_min, delta_m):
+        log_uniform_prior = - np.log(np.prod(self.cubesize))
+        log_jeffreys_prior = - 0.5*np.log(rate)
+        return log_uniform_prior + log_jeffreys_prior
--- cogwheel/population_inference/pop_utils.py
+++ cogwheel/population_inference/pop_utils.py
@@ -3,6 +3,7 @@ functions that are useful for defining population models
 '''
 
 import numpy as np
+from scipy.special import erf
 
 
 def normalized_powerlaw_distribution(x, alpha, x_min, x_max):
@@ -22,8 +23,38 @@ def normalized_powerlaw_distribution(x, alpha, x_min, x_max):
 # def normalized_truncated_gaussian_distribution(x, mu, sigma, x_min, x_max):
 #     '''
 #     Returns f(x|mu, sigma, x_min, x_max) = A * exp(-(x-mu)^2 / 2*sigma^2)
+#     where A is the normalization
 #     '''
+#     mask_zero = np.where(np.logical_or(x<x_min, x>x_max))[0]
     
+#     A = (np.sqrt(np.pi/(2*sigma**2)) * 
+#          (erf(np.sqrt(sigma**2/2)*(x_max-mu)) - erf(np.sqrt(sigma**2/2)*(x_min-mu))))**(-1)
+#     f_x = np.exp(-(x-mu)**2/(2*sigma**2))
+#     normed_f_x = A*f_x
+#     normed_f_x[mask_zero] = 1e-10
+
+#     return normed_f_x
+
+# def smoothing_function(x, x_min, delta_x):
+#     '''
+#     Returns f(x| x_min, delta_x) = 
+#         0                                for (x < x_min)
+#         [f(x-x_min, delta_x) + 1]^-1     for (x_min <= x < x_min+delta_x)
+#         1                                for (x >= x_min + delta_x)
+        
+#     where f(x', delta_x) = exp(delta_x/x' + delta_x/(x'-delta_x))
+#     '''
+#     mask_zero = np.where(x<x_min)[0]
+#     mask_one = np.where(x>=x_min+delta_x)[0]
+
+#     x_prime = x-x_min
+#     f_x = np.exp(delta_x/x_prime + delta_x/(x_prime - delta_x))
+#     S_x = (f_x + 1)**-1
+    
+#     S_x[mask_zero] = 0
+#     S_x[mask_one] = 1
+
+#     return S_x
 
 def smoothing_function(x, x_min, delta_x):
     '''
@@ -34,13 +65,24 @@ def smoothing_function(x, x_min, delta_x):
         
     where f(x', delta_x) = exp(delta_x/x' + delta_x/(x'-delta_x))
     '''
-    if x<x_min:
-        S_x = 0
-    elif x>= x_min+delta_x:
-        S_x = 1
-    elif x>=x_min and x<(x_min+delta_x):
-        x_prime = x-x_min
-        f_x = np.exp(delta_x/x_prime + delta_x/(x_prime - delta_x))
-        S_x = (f_x + 1)**-1
-    return S_x
+    # Initialize the result with zeros
+    S_x = np.zeros_like(x, dtype=float)
+    
+    # Define the range where x_min <= x < x_min + delta_x
+    mask_middle = (x >= x_min) & (x < x_min + delta_x)
+    
+    # Define x_prime for the middle range
+    x_prime = x[mask_middle] - x_min
+    
+    # Calculate f(x_prime, delta_x) and the smoothing function in the middle range
+    if x_prime.size > 0:  # Ensure there is data in the middle range
+        f_x = np.exp(delta_x / x_prime + delta_x / (x_prime - delta_x))
+        S_x[mask_middle] = (f_x + 1) ** -1
+    
+    # Define the range where x >= x_min + delta_x
+    mask_one = (x >= x_min + delta_x)
+    
+    # Set the smoothing function to 1 for the x >= x_min + delta_x range
+    S_x[mask_one] = 1
 
+    return S_x

7e8b667d0f104c9df3c6bca5209a972fb2331b96
Isha Anantpurkar <isha@braid2.cnsi.ucsb.edu>
2024-10-22 17:47:10 -0700

    removed old comments

--- cogwheel/population_inference/pop_utils.py
+++ cogwheel/population_inference/pop_utils.py
@@ -4,6 +4,7 @@ functions that are useful for defining population models
 
 import numpy as np
 from scipy.special import erf
+# from scipy.integrate import quad, trapezoid
 
 
 def normalized_powerlaw_distribution(x, alpha, x_min, x_max):
@@ -12,49 +13,14 @@ def normalized_powerlaw_distribution(x, alpha, x_min, x_max):
     where A = (1+alpha)/(x_max^(1+alpha) - x_min^(1+alpha))
     '''
     mask_zero = np.where(np.logical_or(x<x_min, x>x_max))[0]
-    
-    A = (1+alpha)/(x_max**(1+alpha) - x_min**(1+alpha))
-    f_x = x**alpha
+
+    A = (1+alpha)/(np.power(x_max, 1+alpha) - np.power(x_min, 1+alpha))
+    f_x = np.power(x, alpha)
     normed_f_x = A*f_x
     normed_f_x[mask_zero] = 1e-10
     
     return normed_f_x
-
-# def normalized_truncated_gaussian_distribution(x, mu, sigma, x_min, x_max):
-#     '''
-#     Returns f(x|mu, sigma, x_min, x_max) = A * exp(-(x-mu)^2 / 2*sigma^2)
-#     where A is the normalization
-#     '''
-#     mask_zero = np.where(np.logical_or(x<x_min, x>x_max))[0]
     
-#     A = (np.sqrt(np.pi/(2*sigma**2)) * 
-#          (erf(np.sqrt(sigma**2/2)*(x_max-mu)) - erf(np.sqrt(sigma**2/2)*(x_min-mu))))**(-1)
-#     f_x = np.exp(-(x-mu)**2/(2*sigma**2))
-#     normed_f_x = A*f_x
-#     normed_f_x[mask_zero] = 1e-10
-
-#     return normed_f_x
-
-# def smoothing_function(x, x_min, delta_x):
-#     '''
-#     Returns f(x| x_min, delta_x) = 
-#         0                                for (x < x_min)
-#         [f(x-x_min, delta_x) + 1]^-1     for (x_min <= x < x_min+delta_x)
-#         1                                for (x >= x_min + delta_x)
-        
-#     where f(x', delta_x) = exp(delta_x/x' + delta_x/(x'-delta_x))
-#     '''
-#     mask_zero = np.where(x<x_min)[0]
-#     mask_one = np.where(x>=x_min+delta_x)[0]
-
-#     x_prime = x-x_min
-#     f_x = np.exp(delta_x/x_prime + delta_x/(x_prime - delta_x))
-#     S_x = (f_x + 1)**-1
-    
-#     S_x[mask_zero] = 0
-#     S_x[mask_one] = 1
-
-#     return S_x
 
 def smoothing_function(x, x_min, delta_x):
     '''
@@ -73,11 +39,11 @@ def smoothing_function(x, x_min, delta_x):
     
     # Define x_prime for the middle range
     x_prime = x[mask_middle] - x_min
-    
+
     # Calculate f(x_prime, delta_x) and the smoothing function in the middle range
     if x_prime.size > 0:  # Ensure there is data in the middle range
         f_x = np.exp(delta_x / x_prime + delta_x / (x_prime - delta_x))
-        S_x[mask_middle] = (f_x + 1) ** -1
+        S_x[mask_middle] = np.power(f_x+1, -1)
     
     # Define the range where x >= x_min + delta_x
     mask_one = (x >= x_min + delta_x)

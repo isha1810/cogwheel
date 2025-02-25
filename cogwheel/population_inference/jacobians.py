3d06e03501d35cf58c392ca11a41067aaae48707
Isha Anantpurkar <isha@braid2.cnsi.ucsb.edu>
2025-02-25 08:50:40 -0800

    minor changes

--- cogwheel/population_inference/jacobians.py
+++ cogwheel/population_inference/jacobians.py
@@ -8,28 +8,28 @@ import numpy as np
 
 def m1_m2_to_m1s_q(m1_source, z):
     """
-    m1, m2 -> m1_source, q
+    (m1, m2) -> (m1_source, q)
     returns log(|det(J)|)
     """
     return np.log(m1_source) + 2*np.log(1+z)
 
 def m1_m2_to_m1s_m2s(z):
     """
-    m1, m2 -> m1_source, m2_source
+    (m1, m2) -> (m1_source, m2_source)
     returns log(|det(J)|)
     """
     return 2*np.log(1+z)
 
 def m1s_m2s_to_m1s_q(m1_source):
     """
-    m1_source, m2_source -> m1_source, q
+    (m1_source, m2_source) -> (m1_source, q)
     returns log(|det(J)|)
     """
     return np.log(m1_source)
 
 def m1s_cumq_to_m1s_q(m1_source, m_min):
     """
-    m1_source, cum_q -> m1_source, q
+    (m1_source, cum_q) -> (m1_source, q)
     returns log(|det(J)|)
     """
     return -np.log(1 - (m_min/m1_source))
@@ -37,13 +37,19 @@ def m1s_cumq_to_m1s_q(m1_source, m_min):
 # ----------------------------------------------------------------------
 # Spin Jacobians
 
-def s1z_s2z_to_chieff_cumchidiff(q, s1z_min=-0.998, s1z_max=0.998):
+def chieff_cumchidiff_to_s1z_s2z(chieff, q, 
+                                 max_spin=0.998):
     """
-    s1z, s2z -> chieff, cumchidiff
+    (chieff, cumchidiff) -> (s1z, s2z)
     returns log(|det(J)|)
     """
-    s1z_range = s1z_max-s1z_min
-    return np.log(s1z_range) + np.log((1+q)/q)
+    delta_q = (1-q)/(1+q)
+    lnJ = np.zeros(len(chieff))
+    mask = abs(chieff)>max_spin*delta_q
+    lnJ[mask] += (np.log(1-delta_q)
+                    - np.log(1-abs(chieff)/max_spin))[mask]
+    return lnJ
+
 
 # def s1z_s2z_to_chieff_chidiff():
 #     """
